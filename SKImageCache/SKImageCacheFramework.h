be12d3b4d45c9287e5c64cd0d9cd96fc5a6a113f
Shin-Kai Chen <skchen@sixnology.com>
2016-03-29 11:04:07 +0800

    Add project

---
+++ SKImageCache/SKImageCacheFramework.h
@@ -0,0 +1,19 @@
+//
+//  SKImageCacheFramework.h
+//  SKImageCacheFramework
+//
+//  Created by Shin-Kai Chen on 2016/3/29.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKImageCache.
+FOUNDATION_EXPORT double SKImageCacheVersionNumber;
+
+//! Project version string for SKImageCache.
+FOUNDATION_EXPORT const unsigned char SKImageCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKImageCache/PublicHeader.h>
+
+
